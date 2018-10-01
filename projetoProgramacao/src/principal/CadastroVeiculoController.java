15b93a15d9bcf063acbf92d6c5d6ec5b492169d3
TheylorMarmitt <TheylorMarmitt@users.noreply.github.com>
2018-10-01 15:46:31 -0300

    Adicionando Combo box Filial em cadastro veiculo

--- projetoProgramacao/src/principal/CadastroVeiculoController.java
+++ projetoProgramacao/src/principal/CadastroVeiculoController.java
@@ -17,91 +17,93 @@ import principal.model.Filial;
 
 public class CadastroVeiculoController {
 
-	 @FXML
-	    private ComboBox<Filial> cbFilial;
+	@FXML
+	private ComboBox<Filial> cbFilial;
 
-	    @FXML
-	    private TextField tfMarca;
+	@FXML
+	private TextField tfMarca;
 
-	    @FXML
-	    private TextField tfModelo;
+	@FXML
+	private TextField tfModelo;
 
-	    @FXML
-	    private TextField tfValor;
+	@FXML
+	private TextField tfValor;
 
-	    @FXML
-	    private TextField tfCor;
+	@FXML
+	private TextField tfCor;
 
-	    @FXML
-	    private DatePicker dtAno;
+	@FXML
+	private DatePicker dtAno;
 
-	    @FXML
-	    private TextField tfPlaca;
+	@FXML
+	private TextField tfPlaca;
 
-	    @FXML
-	    private CheckBox cbkDisponivel;
+	@FXML
+	private CheckBox cbkDisponivel;
 
-	    @FXML
-	    private Button btnCadastrar;
+	@FXML
+	private Button btnCadastrar;
 
-	    @FXML
-	    private Button btnNovo;
-	    
-	    
-	    private Carro carro;
-	    private CarroDAO carroDao = AbstractFactory.get().carroDao();
-	    private FilialDAO filialDao = AbstractFactory.get().filialDao();
-//	    private AquisicaoVeiculosDAO aquisicaoDao = AbstractFactory.get().aquisicaoDao();
-	    
-	    @FXML
-		private void initialize() {
-			populaCombo();
+	@FXML
+	private Button btnNovo;
+
+	private Carro carro;
+	private CarroDAO carroDao = AbstractFactory.get().carroDao();
+	private FilialDAO filialDao = AbstractFactory.get().filialDao();
+	// private AquisicaoVeiculosDAO aquisicaoDao =
+	// AbstractFactory.get().aquisicaoDao();
+
+	@FXML
+	private void initialize() {
+		populaCombo();
+	}
+
+	private void populaCombo() {
+		for (Filial filial : filialDao.listar()) {
+			cbFilial.getItems().add(filial);
 		}
-	    
-	    private void populaCombo(){
-			for(Filial filial: filialDao.listar()){
-				cbFilial.getItems().add(filial);
-			}
-		}
-	    
-	    @FXML
-	    void cadastrar(ActionEvent event) {
-	    	populaCarro();
-	    	carroDao.inserir(carro);
-	    	novoCarro();
-	    }
+	}
 
-	    @FXML
-	    void novo(ActionEvent event) {
-	    	novoCarro();
-	    }
-	    
-	    /**
-	    *popula carro com as entradas do usuario
-	    */
-	    public void populaCarro() {
-	    	carro = new Carro();
-	    	carro.setAno(dtAno.getValue());
-			carro.setCor(tfCor.getText());
-			carro.setDisponivel(cbkDisponivel.isSelected());
-			carro.setMarca(tfMarca.getText());
-			carro.setModelo(tfModelo.getText());
-			carro.setPlaca(tfPlaca.getText());
-			carro.setValor(Double.valueOf(tfValor.getText()));
-			carro.setDataDeAquisicao(LocalDate.now());
-			carro.setFilial(cbFilial.getValue());
+	@FXML
+	void cadastrar(ActionEvent event) {
+		populaCarro();
+		carroDao.inserir(carro);
+		novoCarro();
+	}
+
+	@FXML
+	void novo(ActionEvent event) {
+		novoCarro();
+	}
+
+	/**
+	 * popula carro com as entradas do usuario
+	 */
+	public void populaCarro() {
+		carro = new Carro();
+		carro.setAno(dtAno.getValue());
+		carro.setCor(tfCor.getText());
+		carro.setDisponivel(cbkDisponivel.isSelected());
+		carro.setMarca(tfMarca.getText());
+		carro.setModelo(tfModelo.getText());
+		carro.setPlaca(tfPlaca.getText());
+		carro.setValor(Double.valueOf(tfValor.getText()));
+		carro.setDataDeAquisicao(LocalDate.now());
+		carro.setFilial(cbFilial.getValue());
+	}
+
+	public void novoCarro() {
+		carro = new Carro();
+		tfCor.clear();
+		tfMarca.clear();
+		tfModelo.clear();
+		tfPlaca.clear();
+		tfValor.clear();
+		if (cbkDisponivel.isSelected()) {
+			cbkDisponivel.setSelected(false);
+			;
 		}
-	    public void novoCarro() {
-	    	carro = new Carro();
-	    	tfCor.clear();
-	    	tfMarca.clear();
-	    	tfModelo.clear();
-	    	tfPlaca.clear();
-	    	tfValor.clear();
-	    	if(cbkDisponivel.isSelected()) {
-	    		cbkDisponivel.setSelected(false);;
-	    	}
-	    	cbFilial.getSelectionModel().clearSelection();
-	    }
+		cbFilial.getSelectionModel().clearSelection();
+	}
 
 }
